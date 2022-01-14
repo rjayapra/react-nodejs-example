06d6f1cd7042a9586cb1b5c9205280042bb43584
Radhika Jayaprakash <85903942+rjayapra@users.noreply.github.com>
2022-01-14 13:02:20 -0500

    Update DisplayBoard.js

--- my-app/src/components/DisplayBoard.js
+++ my-app/src/components/DisplayBoard.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export const DisplayBoard = ({numberOfUsers, getAllUsers}) => {
 
-    const headerStyle = {
+    /*const headerStyle = {
 
         width: '100%',
         padding: '2%',
         backgroundColor: "red",
         color: 'white',
         textAlign: 'center'
-    }
+    }*/
     
     return(
         <div style={{backgroundColor:'green'}} className="display-board">
